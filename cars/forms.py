6ced04052ce44da72768b17a194ee25816f0f756
Coquinhaaa <juliano.ostroski@gmail.com>
2024-09-20 23:22:54 -0400

    Alterações para salvar os cadastros de carros no banco de dados

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car, Type
 
 class CarForm(forms.Form):
     model = forms.CharField(max_length=200)
@@ -9,5 +9,18 @@ class CarForm(forms.Form):
     plate = forms.CharField(max_length=10)
     value = forms.FloatField()
     photo = forms.ImageField()
+    type = forms.ModelChoiceField(Type.objects.all())
 
-    
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            value = self.cleaned_data['value'],
+            photo = self.cleaned_data['photo'],
+            type = self.cleaned_data['type'],
+        )
+        car.save()
+        return car
