cfc7bb5115b024762e5216a65fa688d19f143dfe
Coquinhaaa <juliano.ostroski@gmail.com>
2024-09-24 22:46:21 -0400

    Criar classe CarDetailView, ajustar template car_detail.html, criar rota em urls.py para nova view

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 # def cars_view(request):
 #     cars = Car.objects.all().order_by('model')
@@ -71,3 +71,8 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
